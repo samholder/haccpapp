647939194638f6c29555c3f7eb4793ca37f154da
sam holder <samholder@gmail.com>
2013-12-03 23:33:31 +0000

    added security so that all actions require https

--- hacapp.web/hacapp.web/Controllers/HomeController.cs
+++ hacapp.web/hacapp.web/Controllers/HomeController.cs
@@ -8,11 +8,13 @@ namespace hacapp.web.Controllers
 {
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public ActionResult Index()
         {
             return View();
         }
 
+        [AllowAnonymous]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
